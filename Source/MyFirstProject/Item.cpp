2cf53ac5a884444eb8cdc3dbac651af92ce7b6e6
LEESEUNGWON\Leeseungwon <iseungwon239@gmail.com>
2022-11-12 23:05:41 +0900

    Particle system & Sound

--- Source/MyFirstProject/Item.cpp
+++ Source/MyFirstProject/Item.cpp
@@ -3,6 +3,11 @@
 
 #include "Item.h"
 #include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/World.h"
+#include "Sound/SoundCue.h"
 
 // Sets default values
 AItem::AItem()
@@ -13,10 +18,11 @@ AItem::AItem()
 	CollisionVolume = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionVolume"));
 	RootComponent = CollisionVolume;
 
-	CollisionVolume->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
-	CollisionVolume->SetCollisionObjectType(ECollisionChannel::ECC_WorldStatic);
-	CollisionVolume->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
-	CollisionVolume->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(GetRootComponent());
+
+	IdleParticlesComponent = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("IdleParticlesComponent"));
+	IdleParticlesComponent->SetupAttachment(GetRootComponent());
 }
 
 // Called when the game starts or when spawned
@@ -38,6 +44,17 @@ void AItem::Tick(float DeltaTime)
 void AItem::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	UE_LOG(LogTemp, Warning, TEXT("Super::OnOverlapBegin()"));
+
+	if (OverlapParticles)
+	{
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), OverlapParticles, GetActorLocation(), FRotator(0.f), true);
+	}
+	Destroy();
+
+	if (OverlapSound)
+	{
+		UGameplayStatics::PlaySound2D(this, OverlapSound);
+	}
 }
 
 void AItem::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
